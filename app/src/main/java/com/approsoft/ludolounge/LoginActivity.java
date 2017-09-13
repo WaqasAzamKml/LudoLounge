b4a8f747608c81e75792bdfacb49cff955ff8f59
Muhammad Waqas <waqasazamkml@gmail.com>
2017-09-13 15:18:11 +0500

    Initial commit

---
+++ app/src/main/java/com/approsoft/ludolounge/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.approsoft.ludolounge;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton btnFBLogin;
+    CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
+    ProfileTracker profileTracker;
+
+    FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Profile userProfile = Profile.getCurrentProfile();
+            if(userProfile != null){
+                nextActivity(userProfile);
+            }
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null)
+            actionBar.hide();
+
+        callbackManager = CallbackManager.Factory.create();
+
+        btnFBLogin = (LoginButton) findViewById(R.id.login_button);
+        btnFBLogin.setReadPermissions(Arrays.asList("public_profile","user_photos", "user_likes"));
+        btnFBLogin.registerCallback(callbackManager, callback);
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            }
+        };
+        accessTokenTracker.startTracking();
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                nextActivity(currentProfile);
+            }
+        };
+        profileTracker.startTracking();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Profile userProfile = Profile.getCurrentProfile();
+        nextActivity(userProfile);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        profileTracker.stopTracking();
+        accessTokenTracker.stopTracking();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void nextActivity(Profile profile){
+        if(profile!=null){
+            Intent i = new Intent(LoginActivity.this, LoungeActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            this.finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/approsoft/ludolounge/LoungeActivity.java
@@ -0,0 +1,72 @@
+package com.approsoft.ludolounge;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+
+public class LoungeActivity extends AppCompatActivity {
+
+    AccessToken accessToken = AccessToken.getCurrentAccessToken();
+    String[] variantTitles = {
+            "Snakes and Ladders",
+            "Classic Ludo",
+            "Quick Ludo",
+            "Masters Ludo",
+            "Variation 5",
+            "Variation 6"
+    };
+    int[] variantLogoIDs = {
+            R.drawable.snake_and_ladder,
+            R.drawable.snake_and_ladder,
+            R.drawable.snake_and_ladder,
+            R.drawable.snake_and_ladder,
+            R.drawable.snake_and_ladder,
+            R.drawable.snake_and_ladder
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lounge);
+
+        if(accessToken==null){
+            Toast.makeText(this, "Please login first!", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(LoungeActivity.this, LoginActivity.class);
+            startActivity(i);
+            this.finish();
+        }
+
+        Profile userProfile = Profile.getCurrentProfile();
+        String userName = userProfile.getName();
+        setTitle("Welcome "+userName);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.lounge_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                LoginManager.getInstance().logOut();
+                Intent i = new Intent(LoungeActivity.this, LoginActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                finish();
+                break;
+        }
+        return true;
+    }
+}
